0ee9f9965951030c3899c1726a7d7d503f5fbdde
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-25 16:54:44 +0530

    Add files via upload

---
+++ Building page/src/Layout/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import facebookImage from '../assests/Facebook Logo.png'
+import spotifyImage from '../assests/Spotify Logo.png'
+import youtubeImage from '../assests/Youtube.jpg'
+import './Footer.css'
+
+const Footer = (props)=>{
+    return(
+        <div className="mainFooter">
+            <span className="footerImage">The Generics</span>
+            <div className="images">
+                <a href="https://www.youtube.com/" target='blank'><img src={youtubeImage}  alt="Youtube Logo" className="youtube"/></a>
+                <a href="https://www.facebook.com/" target='blank'><img src={spotifyImage} alt="Spotify logo" className="spotify" /></a>
+                <a href="https://www.facebook.com/" target='blank'><img src={facebookImage} alt="Spotify logo" className="spotify" /></a>
+                
+            </div>
+        </div>
+    )
+};
+export default Footer;
