a5e33418ae975b23a4fe201a90bb89dd74725eb1
Shilpi Kumari <56399342+Shilpi21420@users.noreply.github.com>
2023-02-25 22:29:00 +0530

    Add files via upload

---
+++ Implementing About/src/Components/Cart/Cart.js
@@ -0,0 +1,35 @@
+
+import React, { useContext } from "react";
+// import Card from "../../UI/Card";
+import './Cart.css'
+import CartContext from "../Context/cart-contetxt";
+
+const Cart = (Props)=>{
+    const cartElements = [
+        {
+        title: 'Colors',
+        price: 100,
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+        quantity: 2,
+        },
+        {
+        title: 'Black and white Colors',
+        price: 50,
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+        quantity: 3,
+        },
+        {
+        title: 'Yellow and Black Colors',
+        price: 70,
+        imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+        quantity: 1,
+        }
+        ]
+        
+       
+        return(
+            <>
+            </>
+        );
+}
+export default Cart;
